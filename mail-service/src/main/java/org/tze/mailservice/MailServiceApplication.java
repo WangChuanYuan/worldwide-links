888d201706f7cbc56dbcb67aa5014de70d1f598d
WangChuanYuan <390589651@qq.com>
2021-04-11 13:03:12 +0800

    Mail Service

---
+++ mail-service/src/main/java/org/tze/mailservice/MailServiceApplication.java
@@ -0,0 +1,15 @@
+package org.tze.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailServiceApplication.class, args);
+    }
+
+}
--- rule-service/src/main/java/org/tze/ruleservice/RuleServiceApplication.java
+++ rule-service/src/main/java/org/tze/ruleservice/RuleServiceApplication.java
@@ -2,7 +2,13 @@ package org.tze.ruleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +16,15 @@ public class RuleServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RuleServiceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate initRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        // 解决中文乱码
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
     }
 
 }
