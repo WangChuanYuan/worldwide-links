3a97b9290000a382d85a80f3677b59ea652b2a7e
LAPTOP-86UF6N1N\52241 <522416667@qq.com>
2021-04-12 10:58:27 +0800

    add device model

---
+++ device-service/src/main/java/org/tze/deviceservice/model/Device.java
@@ -0,0 +1,103 @@
+package org.tze.deviceservice.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Device {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 项目
+     */
+    private Long projectId;
+
+    /**
+     * 产品
+     */
+    private Long productId;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * Logo
+     */
+    private String logo;
+
+    /**
+     * 地理位置
+     */
+    private String location;
+
+    /**
+     * 型号
+     */
+    private String model;
+
+    /**
+     * 厂家
+     */
+    private String industry;
+
+    /**
+     * 序列号
+     */
+    private String sn;
+    /**
+     *
+     */
+    private Date lastActive;
+
+    /**
+     * 1:在线;2:离线;3未激活
+     */
+    private Integer state;
+
+    /**
+     * MQTT用户名
+     */
+    private String username;
+
+    /**
+     * MQTT ClientID
+     */
+    private String clientId;
+
+    /**
+     * MQTT密码
+     */
+    private String password;
+
+    /**
+     * 是否超级权限
+     */
+    private int isSuperuser;
+
+    /**
+     * 认证token
+     */
+    private String token;
+//
+//    /**
+//     * 参数
+//     */
+//    @TableField(typeHandler = JacksonTypeHandler.class)
+//    private List<FieldParam> fieldParams;
+
+    /**
+     * 描述
+     */
+    private String description;
+    /**
+     *
+     */
+//    @TableField(exist = false)
+//    private String project;
+//    @TableField(exist = false)
+//    private String product;
+}
