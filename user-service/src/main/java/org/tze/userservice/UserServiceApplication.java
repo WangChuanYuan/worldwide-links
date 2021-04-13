069ea4232a3894266a159d113dee684dba86026b
WangMo <wangmo15@foxmail.com>
2021-04-13 14:48:25 +0800

    完成基于mqtt协议的publish和subscribe功能，创建user-service

---
+++ user-service/src/main/java/org/tze/userservice/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package org.tze.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: WangMo
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
