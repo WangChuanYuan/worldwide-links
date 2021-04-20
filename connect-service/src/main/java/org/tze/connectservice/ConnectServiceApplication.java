33c3daef9981df742939602d02f4bbcf8f6095c6
WangMo <wangmo15@foxmail.com>
2021-04-20 14:20:54 +0800

    实现设备断联，提供接口

--- connect-service/src/main/java/org/tze/connectservice/ConnectServiceApplication.java
+++ connect-service/src/main/java/org/tze/connectservice/ConnectServiceApplication.java
@@ -3,10 +3,15 @@ package org.tze.connectservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.tze.connectservice.server.NettyServer;
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
 public class ConnectServiceApplication {
 
     public static void main(String[] args) {
@@ -16,5 +21,7 @@ public class ConnectServiceApplication {
         //启动1883
         new NettyServer().startup();
     }
+
+
 
 }
